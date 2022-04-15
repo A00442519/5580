8b3990ebaa278821c09ca98dded47633180ae960
A00442519 <mayuresh.sawant@smu.ca>
2022-04-15 18:11:33 -0300

    Updated with flag emoji and simplified code

--- A00442519.py
+++ A00442519.py
@@ -1,29 +1,27 @@
 import pandas as pd
 import streamlit as st
 import requests
-from currency_converter import CurrencyConverter
 
-converter = CurrencyConverter(fallback_on_missing_rate=True,
-                              fallback_on_wrong_date=True)
 st.header('Bitcoin Price Tracker')
+
 days = st.slider('No of days', 1, 365)
-currency = st.radio('Currency', ('CAD', 'USD', 'EUR','INR'))
-payload = {'vs_currency': currency, 'days': days, 'interval': 'daily'}
+currency = st.radio('Currency', ('🇨🇦 CAD', '🇺🇸 USD', '🇪🇺 EUR','🇮🇳 INR'))
+
+payload = {'vs_currency': currency.split(' ')[-1], 'days': days, 'interval': 'daily'}
 req = requests.get(
     'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart', params=payload)
+
 if req.status_code == 200:
     r = req.json()
-df = pd.DataFrame(r['prices'], columns=['date', currency])
-df['date'] = pd.to_datetime(df['date'],unit='ms')
-df = df.tail(days)
-if currency == 'USD':
-    df[currency] = df.apply(lambda row: converter.convert(row[currency], 'CAD', 'USD', date=row["date"]), axis=1)
-elif currency=='EUR':
-    df[currency] = df.apply(lambda row: converter.convert(row[currency], 'CAD', 'EUR', date=row["date"]), axis=1)
-elif currency=='INR':
-    df[currency] = df.apply(lambda row: converter.convert(row[currency], 'CAD', 'INR', date=row["date"]), axis=1)
-df = df.set_index('date')
+else:
+    st.write(req.status_code)
+
+df = pd.DataFrame(r['prices'], columns=['Date', currency])
+df['Date'] = pd.to_datetime(df['Date'], unit='ms')
+df = df.set_index('Date')
 mean_price = df[currency].mean()
+
 st.line_chart(df[currency])
-st.write('Average Price during this time was  {} {:.2f}'.format(currency, mean_price))
+st.write('Average Price during the last {} days time was  {} {:,.2f}'.format(
+    days, currency, mean_price))
 st.subheader('Assignment by Mayuresh Sawant (A00442519)')
